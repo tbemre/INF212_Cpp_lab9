2180a3affdd33dda3967fdfb5256e00df0a527e5
tbemre <124307360+tbemre@users.noreply.github.com>
2023-05-13 19:17:07 +0000

    main

--- TASK_1/main.cpp
+++ TASK_1/main.cpp
@@ -3,17 +3,36 @@
 using namespace std;
 class Student {
     public:
-        void SetName();
-        void SetName(string n);
-        string GetName();
-        void SetScore(int[]);
-        void SetAver();
-        float GetAver ();
+        void SetName(){
+            name = "";
+        }
+        void SetName(string n){
+            name = n;
+        }
+        string GetName(){
+            return name;
+        }
+        void SetScore(int arr[]){
+            for(int i=0;i<5;i++){
+                score[i] = *arr++;
+            }
+        }
+        void SetAver(){
+            int sum = 0;
+            for(int i=0;i<5;i++){
+                sum += score[i];
+            }
+            average = sum/5;
+        }
+        float GetAver (){
+            return average;
+        }
     private:
         string name;
         float average;
         int score[5];
 };
+
 int main() {
     Student st1;
     st1.SetName("Mustafa Keser");
@@ -22,4 +41,5 @@ int main() {
     st1.SetScore(marks);
     st1.SetAver();
     cout<<st1.GetAver()<<endl;
+    return 0;
 }
